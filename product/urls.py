6dcbf247bbd9460eef35d0f8e8944b673a5dcebd
Dia-crypto-eng <RNOIR24@GMAIL.COM>
2024-04-11 11:12:47 +0100

    create get_product_by_category

--- product/urls.py
+++ product/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('category',views.get_all_category,name='')
+    path('category',views.get_all_category,name=''),
+    path('<str:category>',views.get_product_by_category,name='')
 ]
--- product/views.py
+++ product/views.py
@@ -1,8 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
 
 
 @api_view(['GET'])
@@ -11,3 +11,11 @@ def get_all_category(request):
     serializer=CategorySerializer(category,many=True)
 
     return Response( serializer.data)
+
+@api_view(['GET'])
+def get_product_by_category(request,category):
+    id=Category.objects.get(name=category)
+    product=Product.objects.filter(category=id)
+    serializer=ProductSerializer(product,many=True)
+    return Response( serializer.data)
+    # return Response( "test")
